07cef72da22d7f26e27140fd1ceadb187f6575c1
Rogert Guevara <inforogert@gmail.com>
2020-02-10 18:46:19 +0100

    comenatio en index de rogert

--- src/main.py
+++ src/main.py
@@ -5,13 +5,14 @@ BEST OF THE BEST
 """
 from flask import Flask
 from flask import url_for
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello Roberto !!'
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
